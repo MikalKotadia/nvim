7e55720b0940202d8133914d10bb8f97237cacba
Mikal Kotadia <mikalkotadia@Mikals-MacBook-Pro.local>
2023-11-23 14:47:10 -0700

    base nvchad config

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+vim.tabstop = 4;
+vim.expandtab = true;
+  
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
---
+++ mappings.lua
@@ -0,0 +1,22 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+
+M.telescope = {
+  n = {
+    ["<leader>ff"] = { ":Telescope git_files<cr>", "Search git files", opts = { nowait = true } },
+    ["<leader>fa"] = { ":Telescope find_files<cr>", "Search all files", opts = { nowait = true } },
+  },
+}
+-- more keybinds!
+
+return M
