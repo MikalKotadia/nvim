2f9fb649c4156f7df72a63a77426a174796267e9
MikalKotadia <mikal.m.kotadia@gmail.com>
2024-03-11 10:52:54 -0600

    new mappings

--- mappings.lua
+++ mappings.lua
@@ -1,40 +1,51 @@
----@type MappingsTable
+--@type MappingsTable
 
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
+    n = {
+        [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    },
+    v = {
+        [">"] = { ">gv", "indent" },
+    },
 }
 
-
 M.telescope = {
-  n = {
-    ["<leader>fg"] = { ":Telescope git_files<cr>", "Search git files", opts = { nowait = true } },
-    ["<leader>fs"] = { ":lua require('telescope.builtin').lsp_document_symbols({ symbols={'function', 'class', 'method'} })<cr>", "View all functions", opts = { nowait = true } },
-  },
+    n = {
+        ["<leader>fg"] = { ":Telescope git_files<cr>", "Search git files", opts = { nowait = true } },
+        ["<leader>fs"] = {
+            ":lua require('telescope.builtin').lsp_document_symbols({ symbols={'function', 'class', 'method'} })<cr>",
+            "View all functions",
+            opts = { nowait = true },
+        },
+    },
 }
 
 M.lazygit = {
-  n = {
-    ["<leader>lg"] = { ":LazyGit<cr>", "Open lazygit", opts = { nowait = true } }
-  },
+    n = {
+        ["<leader>lg"] = { ":LazyGit<cr>", "Open lazygit", opts = { nowait = true } },
+    },
 }
 
 M.case_formatting = {
-  v = {
-    ["<leader>tc"] = { ":s/\\%V\\<./\\u&/g<cr>", "convert current line to title case", opts = { nowait = true } }
-  },
+    v = {
+        ["<leader>tc"] = { ":s/\\%V\\<./\\u&/g<cr>", "convert current line to title case", opts = { nowait = true } },
+    },
 }
 M.code_running = {
-  n = {
-    ["<leader>rp"] = { ':!python "%"<cr>', "Run the current buffer in python"}
-  },
+    n = {
+        ["<leader>rp"] = { ':!python "%"<cr>', "Run the current buffer in python" },
+    },
 }
+
+M.chatgpt = {
+    v = {
+        ["<leader>ds"] = {':ChatGPTRun docstring<cr>', 'generate docstring with chatgpt'},
+    },
+}
+
+
 -- more keybinds!
 
 return M
