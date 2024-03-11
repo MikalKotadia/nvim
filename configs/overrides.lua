20e04ed7058e91f6cc6d6b4fce4805ff520df79a
MikalKotadia <mikal.m.kotadia@gmail.com>
2024-03-11 10:54:35 -0600

    removed comment

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -74,8 +74,4 @@ M.nvimtree = {
   },
 }
 
--- overide for adding react comments
--- M.comments = {
---   pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
--- }
 return M
