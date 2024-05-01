c1c9af7f5db3d7a4a4c3b5ac25786e7fef6e1206
MIkal Kotdia <mikalkotdia@MIkals-MacBook-Pro.local>
2024-05-01 16:27:20 -0600

    added options back

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,16 @@
 require "nvchad.options"
 
 -- add yours here!
+--
+local opt = vim.opt
+-- local autocmd = vim.api.nvim_create_autocmd
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+-- don't add comment on new line
+vim.cmd [[autocmd FileType * set formatoptions-=ro]]
